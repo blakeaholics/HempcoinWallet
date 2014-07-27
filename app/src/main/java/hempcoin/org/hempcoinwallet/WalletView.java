c78f939831db487b2556c956e0f99a79ecfa5501
blakeaholics <blakeaholics@gmail.com>
2014-07-26 17:39:29 -0700

    Updated 0.0.2
    Bunch of tweaks, improved Webview usage.
    More to come!

--- app/src/main/java/hempcoin/org/hempcoinwallet/WalletView.java
+++ app/src/main/java/hempcoin/org/hempcoinwallet/WalletView.java
@@ -3,9 +3,12 @@ package hempcoin.org.hempcoinwallet;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,6 +20,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class WalletView extends Activity {
@@ -48,6 +52,24 @@ public class WalletView extends Activity {
             webView.loadUrl("http://54.186.168.133/");
         }
     }
+	
+	@Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(event.getAction() == KeyEvent.ACTION_DOWN){
+            switch(keyCode)
+            {
+            case KeyEvent.KEYCODE_BACK:
+                if(webView.canGoBack()){
+                    webView.goBack();
+                }else{
+                    finish();
+                }
+                return true;
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 
     @Override
     protected void onSaveInstanceState(Bundle outState )
@@ -76,10 +98,10 @@ public class WalletView extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_about) {
             AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
             builder1.setTitle("About the Hempcoin Wallet");
-            builder1.setMessage("The (Simple) Hempcoin Wallet - v0.0.1\n" +
+            builder1.setMessage("The (Simple) Hempcoin Wallet     - v0.0.2\n" +
                     "\n" +
                     "This application is simply a web-wrapper for the online Hempcoin wallet. We have created this application to make access much faster and simpler.\n" +
                     "\n" +
@@ -97,6 +119,52 @@ public class WalletView extends Activity {
             builder1.setIcon(R.drawable.ic_launcher);
             AlertDialog alert11 = builder1.create();
             alert11.show();
+            return true;
+        } else if (id == R.id.action_settings) {
+            AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
+            builder1.setTitle("Settings");
+            builder1.setMessage("Coming soon, including:" +
+                    "\n" +
+                    " * Improved WebView usage." +
+                    "\n" +
+                    " * Secure and proper credentials management." +
+                    "\n" +
+                    "& more.\n" +
+                    "\n" +
+                    "For help, contact dev@hempcoin.org");
+            builder1.setCancelable(true);
+            builder1.setNeutralButton(android.R.string.ok,
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    }
+            );
+
+            //builder1.setIcon(R.drawable.ic_launcher);
+            AlertDialog alert11 = builder1.create();
+            alert11.show();
+            return true;
+        } else if (id == R.id.action_donate) {
+            ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+            ClipData clip = ClipData.newPlainText("donate", "HC9AwhcakbKNtmYiaqpELHVWvTnEHJ66tp");
+            clipboard.setPrimaryClip(clip);
+
+            Context context = getApplicationContext();
+            CharSequence text = "Copied donation address to clipboard.";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+            return true;
+        } else if (id == R.id.action_faucet) {
+            webView.loadUrl("http://sillypothead.com/thcfaucet/");
+            Context context = getApplicationContext();
+            CharSequence text = "Taking you to SillyPothead's faucet...";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
             return true;
         }
         return super.onOptionsItemSelected(item);
